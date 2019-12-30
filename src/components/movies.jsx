531f0db7eba64dfb0f76ea42b1e0903f35d31249
Usman Haider <uhaider.bese15seecs@seecs.edu.pk>
2019-12-30 21:36:00 +0500

    movies table moved to its own component

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
-import Like from "./common/like";
 import ListGroup from "./common/listGroup";
 import Pagination from "./common/pagination";
 import paginate from "./utils/paginate";
+import MoviesTable from "./moviesTable";
 
 class Movies extends Component {
   render() {
@@ -13,7 +13,9 @@ class Movies extends Component {
       onPageChange,
       genres,
       onGenreSelect,
-      selectedGenre
+      selectedGenre,
+      onLike,
+      onDelete
     } = this.props;
 
     const { length: count } = this.props.movies;
@@ -40,43 +42,12 @@ class Movies extends Component {
         </div>
         <div className="col">
           <p>There are {filteredMovies.length} movies in database</p>
-          <table className="table">
-            <thead>
-              <tr>
-                <th scope="col">Title</th>
-                <th scope="col">Genre</th>
-                <th scope="col">Stock</th>
-                <th scope="col">Rate</th>
-                <th></th>
-                <th></th>
-              </tr>
-            </thead>
-            <tbody>
-              {movies.map(movie => (
-                <tr key={movie._id}>
-                  <td> {movie.title} </td>
-                  <td> {movie.genre.name} </td>
-                  <td> {movie.numberInStock} </td>
-                  <td> {movie.dailyRentalRate} </td>
-                  <td>
-                    <Like
-                      liked={movie.liked}
-                      onClick={() => this.props.onLike(movie)}
-                      movie={movie}
-                    ></Like>
-                  </td>
-                  <td>
-                    <button
-                      className="btn btn-danger"
-                      onClick={() => this.props.onDelete(movie._id)}
-                    >
-                      Delete
-                    </button>
-                  </td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
+
+          <MoviesTable
+            movies={movies}
+            onLike={onLike}
+            onDelete={onDelete}
+          ></MoviesTable>
           <Pagination
             itemCount={filteredMovies.length}
             pageSize={pageSize}

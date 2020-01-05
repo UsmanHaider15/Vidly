b5f37227c980e9e4aa9730fa4a60760507afb619
Usman Haider <uhaider.bese15seecs@seecs.edu.pk>
2020-01-05 21:32:01 +0500

    validation errors

--- src/components/common/loginForm.jsx
+++ src/components/common/loginForm.jsx
@@ -6,11 +6,20 @@ class LoginForm extends Component {
     account: {
       username: "",
       password: ""
-    }
+    },
+    errors: {}
   };
 
-  handleSubmit = () => {
-    console.log("form submitted");
+  validate = () => {
+    return { username: "Username is required  " };
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    const errors = this.validate();
+    this.setState({ errors });
+    if (errors) return;
   };
 
   handleChange = ({ currentTarget: input }) => {

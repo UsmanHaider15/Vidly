0b4e275e2ce3347c0fc399398c6e25ab55c07642
Usman Haider <uhaider.bese15seecs@seecs.edu.pk>
2020-01-12 15:13:52 +0500

    Handling login errors

--- src/components/common/loginForm.jsx
+++ src/components/common/loginForm.jsx
@@ -22,8 +22,17 @@ class LoginForm extends Form {
   };
 
   doSubmit = async () => {
-    const { data } = this.state;
-    await authService.login(data.username, data.password);
+    try {
+      const { data } = this.state;
+      await authService.login(data.username, data.password);
+    } catch (error) {
+      if (error.response && error.response.status === 400) {
+        const errors = { ...this.state.errors };
+        errors.username = error.response.data;
+
+        this.setState({ errors });
+      }
+    }
   };
 
   render() {

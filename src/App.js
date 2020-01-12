21ee72b989cb75725887d42cff0941ce7f9262ec
Usman Haider <uhaider.bese15seecs@seecs.edu.pk>
2020-01-12 17:01:32 +0500

    logging out a User

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import MovieForm from "./components/movieForm";
 import LoginForm from "./components/common/loginForm";
 import RegistrationFrom from "./components/registrationForm";
 import "react-toastify/dist/ReactToastify.css";
+import Logout from "./components/common/logout";
 
 class App extends Component {
   state = {};
@@ -31,6 +32,7 @@ class App extends Component {
         <main className="container">
           <Switch>
             <Route path="/login" component={LoginForm} />
+            <Route path="/logout" component={Logout} />
             <Route path="/register" component={RegistrationFrom} />
             <Route path="/movies/:id" component={MovieForm} />
             <Route path="/movies" render={props => <Movies />} />
